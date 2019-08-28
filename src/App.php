061b8ef5315f9b3cbec7b9798da6a585f0cec931
77web <info@77-web.com>
2019-08-28 18:47:50 +0900

    initial commit

---
+++ src/App.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Quartetcom\Nagoyaphp17;
+
+class App
+{
+}
---
+++ tests/AppTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Quartetcom\Nagoyaphp17;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+
+}
