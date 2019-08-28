808908b97e280bfee7e52b099a6f508288a18c40
77web <info@77-web.com>
2019-08-28 19:18:14 +0900

    とりあえず速度を考えない実装

--- src/App.php
+++ src/App.php
@@ -6,4 +6,28 @@ namespace Quartetcom\Nagoyaphp17;
 
 class App
 {
+    /**
+     * @var TripleNumberSpec
+     */
+    private $spec;
+
+    /**
+     * @param TripleNumberSpec $spec
+     */
+    public function __construct(TripleNumberSpec $spec)
+    {
+        $this->spec = $spec;
+    }
+
+    public function run(string $input)
+    {
+        $cursor = intval($input);
+
+        do {
+            $cursor++;
+            $converted = sprintf('%b', $cursor);
+        } while ($this->spec->isSatisfiedBy($converted));
+
+        return (string)$cursor;
+    }
 }
